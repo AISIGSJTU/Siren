1f15e861b025904375bd5dbd28887fc39f2a2b48
Hanxi Guo <ghx171@126.com>
2021-09-19 16:15:22 +0800

    add resnet-18 support

--- utils/mnist.py
+++ utils/mnist.py
@@ -1,13 +1,16 @@
 #########################
-# Purpose: Model definitions and other utlities for MNIST and Fashion MNIST
+# Purpose: Model definitions and other utlities for MNIST, Fashion MNIST, 
 ########################
 
+import tensorflow as tf
 from keras.datasets import mnist
-from keras.models import Sequential, model_from_json
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential, model_from_json, Model
 from keras.layers import Dense, Dropout, Activation, Flatten, Input
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, MaxPool2D, GlobalAveragePooling2D, Layer, Add
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
+import global_vars as gv
 
 from utils.fmnist import load_fmnist
 import global_vars as gv
@@ -63,6 +66,8 @@ def data_mnist(one_hot=True):
         return X_train, y_train, X_test, y_test, Server_X, Server_Y
     else:
         return X_train, y_train, X_test, y_test
+
+
 
 def model_cifar():
     model = Sequential()
@@ -242,13 +247,104 @@ def model_LR():
 
     return model
 
+class ResnetBlock(Model):
+    """
+    A standard resnet block.
+    """
+
+    def __init__(self, channels: int, down_sample=False):
+        """
+        channels: same as number of convolution kernels
+        """
+        super().__init__()
+
+        self.__channels = channels
+        self.__down_sample = down_sample
+        self.__strides = [2, 1] if down_sample else [1, 1]
+
+        KERNEL_SIZE = (3, 3)
+        # use He initialization, instead of Xavier (a.k.a 'glorot_uniform' in Keras), as suggested in [2]
+        INIT_SCHEME = "he_normal"
+
+        self.conv_1 = Conv2D(self.__channels, strides=self.__strides[0],
+                             kernel_size=KERNEL_SIZE, padding="same", kernel_initializer=INIT_SCHEME)
+        self.bn_1 = BatchNormalization()
+        self.conv_2 = Conv2D(self.__channels, strides=self.__strides[1],
+                             kernel_size=KERNEL_SIZE, padding="same", kernel_initializer=INIT_SCHEME)
+        self.bn_2 = BatchNormalization()
+        self.merge = Add()
+
+        if self.__down_sample:
+            # perform down sampling using stride of 2, according to [1].
+            self.res_conv = Conv2D(
+                self.__channels, strides=2, kernel_size=(1, 1), kernel_initializer=INIT_SCHEME, padding="same")
+            self.res_bn = BatchNormalization()
+
+    def call(self, inputs):
+        res = inputs
+
+        x = self.conv_1(inputs)
+        x = self.bn_1(x)
+        x = tf.nn.relu(x)
+        x = self.conv_2(x)
+        x = self.bn_2(x)
+
+        if self.__down_sample:
+            res = self.res_conv(res)
+            res = self.res_bn(res)
+
+        # if not perform down sample, then add a shortcut directly
+        x = self.merge([x, res])
+        out = tf.nn.relu(x)
+        return out
+
+
+class ResNet18(Model):
+
+    def __init__(self, num_classes, **kwargs):
+        """
+            num_classes: number of classes in specific classification task.
+        """
+        super().__init__(**kwargs)
+        self.conv_1 = Conv2D(64, (7, 7), strides=2,
+                             padding="same", kernel_initializer="he_normal")
+        self.init_bn = BatchNormalization()
+        self.pool_2 = MaxPool2D(pool_size=(2, 2), strides=2, padding="same")
+        self.res_1_1 = ResnetBlock(64)
+        self.res_1_2 = ResnetBlock(64)
+        self.res_2_1 = ResnetBlock(128, down_sample=True)
+        self.res_2_2 = ResnetBlock(128)
+        self.res_3_1 = ResnetBlock(256, down_sample=True)
+        self.res_3_2 = ResnetBlock(256)
+        self.res_4_1 = ResnetBlock(512, down_sample=True)
+        self.res_4_2 = ResnetBlock(512)
+        self.avg_pool = GlobalAveragePooling2D()
+        self.flat = Flatten()
+        self.fc = Dense(num_classes, activation="softmax")
+
+    def call(self, inputs):
+        out = self.conv_1(inputs)
+        out = self.init_bn(out)
+        out = tf.nn.relu(out)
+        out = self.pool_2(out)
+        for res_block in [self.res_1_1, self.res_1_2, self.res_2_1, self.res_2_2, self.res_3_1, self.res_3_2, self.res_4_1, self.res_4_2]:
+            out = res_block(out)
+        out = self.avg_pool(out)
+        out = self.flat(out)
+        out = self.fc(out)
+        return out
+
+def model_resnet18():
+    model = ResNet18(gv.NUM_CLASSES)
+    return model
+
 
 def model_mnist(type):
     """
     Defines MNIST model using Keras sequential model
     """
 
-    models = [model_cifar, modelA, modelB, modelC, modelD, modelE, modelF, modelG, model_LR]
+    models = [model_cifar, modelA, modelB, modelC, modelD, modelE, modelF, modelG, model_LR, model_resnet18]
 
     return models[type]()
 
